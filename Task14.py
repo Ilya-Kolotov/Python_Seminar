406c4a57f126dc0c4b1fce1d9a1a7f1b96d71ca1
Ilya.Kolotov <kolotov.iljya@gmail.com>
2023-01-13 01:08:34 +0500

    Update Seminar 4

--- Task14.py
+++ Task14.py
@@ -19,61 +19,61 @@ print(numbers)
 print(max_count)
 
 # 2
-import random
+# import random
 
-max_count = 0
-numbers = list()
-for i in range(10):
-    numbers.append(random.randint(2,5))
-print(numbers)
-i=0
-while i<len(numbers)-max_count:
-    count = 1
-    for j in range(i+1,len(numbers)):
-        if numbers[i] == numbers[j]:
-            count += 1
+# max_count = 0
+# numbers = list()
+# for i in range(10):
+#     numbers.append(random.randint(2,5))
+# print(numbers)
+# i=0
+# while i<len(numbers)-max_count:
+#     count = 1
+#     for j in range(i+1,len(numbers)):
+#         if numbers[i] == numbers[j]:
+#             count += 1
     
-    if count > max_count:
-        max_count = count
-    i+=1
-print(max_count)
+#     if count > max_count:
+#         max_count = count
+#     i+=1
+# print(max_count)
 
 
-import random
+# import random
 
-a = 1
-b = 6
+# a = 1
+# b = 6
 
-array = [random.randint(a, b) for i in range(20)]
-print(array)
+# array = [random.randint(a, b) for i in range(20)]
+# print(array)
 
-d = {}
+# d = {}
 
-for i in array:
-    if i in d:
-        d[i] += 1
-    else:
-        d[i] = 1
+# for i in array:
+#     if i in d:
+#         d[i] += 1
+#     else:
+#         d[i] = 1
     
-print(d)
+# print(d)
 
-max = 0
-for i in range(a, (b+1)):
-    if(d[i] > max):
-        max = d[i]
+# max = 0
+# for i in range(a, (b+1)):
+#     if(d[i] > max):
+#         max = d[i]
 
-print(f"Максимальное повторение элемента {max} раз.")
+# print(f"Максимальное повторение элемента {max} раз.")
 
-import random
+# import random
 
-list_num = []
-max = 0
-for i in range(10):
-    a= random.randint(2,5)
-    list_num.append(a)
-print(list_num)
-print(set(list_num))
-for i in set(list_num):
-    if list_num.count(i)>max:
-        max = list_num.count(i)
-print(max)
+# list_num = []
+# max = 0
+# for i in range(10):
+#     a= random.randint(2,5)
+#     list_num.append(a)
+# print(list_num)
+# print(set(list_num))
+# for i in set(list_num):
+#     if list_num.count(i)>max:
+#         max = list_num.count(i)
+# print(max)
